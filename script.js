66a0f7b89ce66f23f7c0ba08991ce8ed8eada447
mummj <mummj@eou.edu>
2021-10-19 09:51:51 -0700

    created the javascirpt demo

---
+++ script.js
@@ -0,0 +1,13 @@
+
+console.log('My script is working');
+
+document.write("<h1>This is my JavaScript Demo</h1>");
+
+document.write("<h3>This was created in JavaScript not HTML </h3>")
+
+alert('This entire page was created by using only JS');
+alert('Here a second alert');
+alert('are you tired of these yet?');
+
+prompt('do you like alerts?');
+prompt('Tell my why?')
